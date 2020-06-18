26c6760993791637efd3a7900665c37d654576c3
Golubovic <ag185178@ncr.com>
2020-06-18 11:49:58 +0200

    Adding files for webapp if it si a new year

---
+++ application.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/more")
+def more():
+    now = datetime.datetime.now()
+    new_year = now.month == 1 and now.day == 1
+    return render_template("more.html", new_year=new_year)
